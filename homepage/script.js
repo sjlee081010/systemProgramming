4a211622e83547936bd911d3c7df66c6531f7439
sjee081010 <sjlee081010@gmail.com>
2025-05-15 15:05:40 +0900

    feat

--- homepage/script.js
+++ homepage/script.js
@@ -1,18 +1,43 @@
 const main = document.querySelector('main');
 const section = document.querySelectorAll('section');
-let page = 0;
+const btn = document.querySelectorAll('.btn');
+
+const info = {};
+
+Object.defineProperties(info, {
+  page: {
+    get() {
+      return this._page || 0;
+    },
+    set(value) {
+      if (value < 0) value = 0;
+      if (value > section.length - 1) value = section.length - 1;
+
+      this._page = value;
+      console.log(value);
+
+      main.scrollLeft = value * main.clientWidth;
+
+      btn.forEach((obj, index) => {
+        obj.style.opacity = index === value ? "100%" : "50%";
+      });
+    },
+    enumerable: true,
+    configurable: true,
+  }
+});
 
 main.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    if (e.deltaY > 0) {
-        page++;
-    } else if (e.deltaY < 0) {
-        page--;
-    }
-    if (page < 0) {
-        page = 0;
-    } else if (page > section.length-1) {
-        page = section.length-1;
-    }
-    main.scrollLeft = page * main.clientWidth;
+  e.preventDefault();
+  if (e.deltaY > 0) {
+    info.page = info.page + 1;
+  } else {
+    info.page = info.page - 1;
+  }
+});
+
+btn.forEach((obj, index) => {
+  obj.addEventListener('click', () => {
+    info.page = index;
+  });
 });
